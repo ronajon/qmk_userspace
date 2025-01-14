70705174f2f353cffe8f4d0a686c0dc097b1b983
ronajon@mailbox.org <ronajon@mailbox.org>
2025-01-14 19:20:10 +0100

    removed splitkb and xtips folders from keyboards folder locally

--- keyboards/splitkb/kyria/keymaps/imfinity/rules.mk
+++
@@ -1,5 +0,0 @@
-OLED_ENABLE = yes
-ENCODER_ENABLE = no       # Enables the use of one or more encoders
-RGB_MATRIX_ENABLE = no     # Disable keyboard RGB matrix, as it is enabled by default on rev3
-RGBLIGHT_ENABLE = no     # Enable keyboard RGB underglow
-TAP_DANCE_ENABLE = yes		# enable Tap Dance
