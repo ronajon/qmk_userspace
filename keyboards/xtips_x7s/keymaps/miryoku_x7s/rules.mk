9f561c2775d0adadb0a93afd24e226c58876ab0f
Ronald Jonkman <ronald.jonkman@sioux.eu>
2025-01-15 13:12:39 +0100

    reset delete of kyria and xtips_x7s

---
+++ keyboards/xtips_x7s/keymaps/miryoku_x7s/rules.mk
@@ -0,0 +1,14 @@
+# needed for 64mb chips
+EEPROM_DRIVER=transient
+
+# from miryoku 
+MOUSEKEY_ENABLE = yes
+EXTRAKEY_ENABLE = yes
+AUTO_SHIFT_ENABLE = yes
+TAP_DANCE_ENABLE = yes
+CAPS_WORD_ENABLE = yes
+KEY_OVERRIDE_ENABLE = yes
+
+# disable RGB and underglow
+RGBLIGHT_ENABLE = no
+BACKLIGHT_ENABLE = no
